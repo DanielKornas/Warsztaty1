5e8e41d59dd3f5b3b24411edf31361ad3787d0f7
Daniel Kornas <danielkornas@gmail.com>
2016-11-16 17:15:11 +0100

    Poprawilem kod, który zrobiłem na zajęciach.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var menuSwitch = document.getElementById('menuSwitch');
-    var menuSwitch2 = document.getElementById('menuSwitch2');
+    var menuSwitch2 = menuSwitch.querySelector('#menuSwitch2');
     var box_first = document.querySelector(".box_first");
     var box_second = document.querySelector(".box_second");
     var transparent = document.querySelectorAll(".transparent");
@@ -11,14 +11,22 @@ document.addEventListener("DOMContentLoaded", function() {
     var panel_left = document.querySelector(".panel_left");
     var panel_right = document.querySelector(".panel_right");
     var sum_value = document.getElementById('sum_value');
+    var transport = document.getElementById('transport');
+    var clickCounter = 0;
     var wholeSum = 0;
-    var transport = document.getElementById('transport')
+
+    // wysuwane menu
+
     menuSwitch.addEventListener("mouseover", function() {
         menuSwitch2.classList.add("menu-show");
     });
     menuSwitch.addEventListener("mouseout", function() {
         menuSwitch2.classList.remove("menu-show");
     });
+
+
+    // efekty najechania na boxy
+
     box_first.addEventListener("mouseover", function() {
         transparent[0].classList.add("transparent-hide");
     });
@@ -31,52 +39,63 @@ document.addEventListener("DOMContentLoaded", function() {
     box_second.addEventListener("mouseout", function() {
         transparent[1].classList.remove("transparent-hide");
     });
-    // event.detail podaje ilosc klikniec naszego event
-    slider_click[0].addEventListener("click", function(first) {
 
-        if (first.detail === 1) {
-            imgSlider.setAttribute("src", "images/red.png");
-        } else if (first.detail === 2) {
-            imgSlider.setAttribute("src", "images/orange.png");
-        } else if (first.detail === 3) {
+    // slider
+
+    slider_click[0].addEventListener("click", function() {
+        clickCounter += 1; // po klikniecuu zwiekszamy licznik i na tej podstawie wyswietla sie wlasciwy obrazek
+        if (clickCounter <= 2) {
+            if (clickCounter === 1) {
+                imgSlider.setAttribute("src", "images/red.png");
+            } else if (clickCounter === 2) {
+                imgSlider.setAttribute("src", "images/orange.png");
+            }
+        } else {
+            clickCounter = 0; // zerowanie licznika zeby wlasciwie pokazywac obrazki
+            imgSlider.setAttribute("src", "images/chair1.png");
+        };
+        console.log(clickCounter);
+    });
+    slider_click[1].addEventListener("click", function() {
+        clickCounter += 1;
+        if (clickCounter <= 2) {
+            if (clickCounter === 1) {
+                imgSlider.setAttribute("src", "images/red.png");
+            } else if (clickCounter === 2) {
+                imgSlider.setAttribute("src", "images/orange.png");
+            }
+        } else {
+            clickCounter = 0;
             imgSlider.setAttribute("src", "images/chair1.png");
         }
-        first.detail === 0; // kliknieto 3 x wiec zeruje ilosc klikniec i licze od nowa , spelniajac powyzsze warunki
-
+        console.log(clickCounter);
     });
-    slider_click[1].addEventListener("click", function(second) {
 
-        if (second.detail === 1) {
-            imgSlider.setAttribute("src", "images/orange.png");
-        } else if (second.detail === 2) {
-            imgSlider.setAttribute("src", "images/red.png");
-        } else if (second.detail === 3) {
-            imgSlider.setAttribute("src", "images/chair1.png");
-        }
-        first.detail === 0;
+    // zamowienie - boxy, kalkulator
 
-    });
     for (var i = 0; i < list_arrow.length; i++) {
+
+        // box wybierz rodzaj
         list_arrow[0].addEventListener("click", function() { // event dla pierwszego boxu , dlatego dla pierwszego ze chce wstawiac wartosc do pierwszego wiersza. pierwszy box z lewej = pierwszy box do wstawienia
             this.nextElementSibling.classList.toggle("list_panel_visible"); // dodajemy widocznosc dla ul z wysuwana lista
             for (var j = 0; j < this.nextElementSibling.children.length; j++) { // dlugosc wszysttkich li, ktore zawieraja wartosci
                 this.nextElementSibling.children[j].addEventListener("click", function(specifyProduct) { // event na konkretne li, this, na element ktory wlasnie klikam
-                    panel_left.children[0].innerHTML = this.dataset.product;
-                    panel_right.children[0].innerHTML = this.dataset.price;
-
-
-
+                    panel_left.children[0].innerHTML = this.dataset.product; // pobieranie nazwy z dataset
+                    panel_right.children[0].innerHTML = this.dataset.price; // pobieranie ceny z dataset
 
                     var productSum = panel_right.children[0].innerHTML;
                     sum_value.innerHTML = Number(productSum);
                     productSum = Number(sum_value.innerHTML); // zapisuje wartosc do zmiennej zeby moc potem dodac
+
                     // event na transport, musi byc tu zeby ladnie sie sumowala suma do zaplaty w zaleznosci zarowno od tego czy wybranu produkt oraz od tego czy zaznaczono transport
-                    transport.parentElement.classList.add("transport-visible"); // transport byl niewidoczny zanim nie wybralo sie produktu, pojawia sie jak sie produkt wybierze
-                    transport.checked = false; // po kazdym nowym kliknieciu w produkt transport sie odznacza - bo inaczej jest zla suma
-                    panel_left.children[3].innerHTML = ""; // i kasuje sie transport ..
-                    panel_right.children[3].innerHTML = ""; //..po to zeby wymusic na uzytkowniku kolejna akcje, kolejne klikniecie w transport i wtedy robi sie dobra suma
-                    transport.addEventListener("click", function(event) {
-                        var checkbox = event.target; // event target - bardzo wazne!!!
+                    transport.parentElement.classList.add("transport-visible"); // transport byl niewidoczny zanim nie wybralo sie produktu, pojawia sie po wybraniu produktu
+                    // po kazdym nowym kliknieciu w produkt transport sie odznacza - zeby zapewnic odpowiednia weryfikacje
+                    transport.checked = false;
+                    panel_left.children[3].innerHTML = "";
+                    panel_right.children[3].innerHTML = "";
+
+                    transport.addEventListener("click", function(event) { // event na zaznaczenie transportu
+                        var checkbox = event.target; // event target - wazne
                         if (checkbox.checked) {
                             panel_left.children[3].innerHTML = "Transport";
                             panel_right.children[3].innerHTML = transport.getAttribute("data-transport-price");
@@ -88,33 +107,26 @@ document.addEventListener("DOMContentLoaded", function() {
                             wholeSum = productSum + Number(panel_right.children[3].innerHTML);
                             sum_value.innerHTML = wholeSum + " zł";
                         }
-
                     });
-
                 });
                 // dodawanie podswietlenia po najechaniu na dana opcje
-
                 this.nextElementSibling.children[j].addEventListener("mouseover", function(specifyProduct) {
                     this.classList.add("product-show");
                 });
                 this.nextElementSibling.children[j].addEventListener("mouseout", function(specifyProduct) {
                     this.classList.remove("product-show");
                 });
-
-
-
             }
         });
 
-        list_arrow[1].addEventListener("click", function() { // event dla pierwszego boxu , dlatego dla pierwszego ze chce wstawiac wartosc do pierwszego wiersza. pierwszy box z lewej = pierwszy box do wstawienia
+        // box wybierz kolor
+        list_arrow[1].addEventListener("click", function() { // event dla drugiego boxa
             this.nextElementSibling.classList.toggle("list_panel_visible"); // dodajemy widocznosc dla ul z wysuwana lista
             for (var j = 0; j < this.nextElementSibling.children.length; j++) { // dlugosc wszysttkich li, ktore zawieraja wartosci
                 this.nextElementSibling.children[j].addEventListener("click", function() { // event na konkretne li, this, na element ktory wlasnie klikam
                     panel_left.children[1].innerHTML = this.dataset.color
                     panel_right.children[1].innerHTML = "0";
                 });
-                // dodawanie podswietlenia po najechaniu na dana opcje
-
                 this.nextElementSibling.children[j].addEventListener("mouseover", function(specifyProduct) {
                     this.classList.add("product-show");
                 });
@@ -124,6 +136,8 @@ document.addEventListener("DOMContentLoaded", function() {
             }
 
         });
+
+        // box wybierz materiał
         list_arrow[2].addEventListener("click", function() { // event dla pierwszego boxu , dlatego dla pierwszego ze chce wstawiac wartosc do pierwszego wiersza. pierwszy box z lewej = pierwszy box do wstawienia
             this.nextElementSibling.classList.toggle("list_panel_visible"); // dodajemy widocznosc dla ul z wysuwana lista
             for (var j = 0; j < this.nextElementSibling.children.length; j++) { // dlugosc wszysttkich li, ktore zawieraja wartosci
@@ -131,8 +145,6 @@ document.addEventListener("DOMContentLoaded", function() {
                     panel_left.children[2].innerHTML = this.dataset.material;
                     panel_right.children[2].innerHTML = "0";
                 });
-                // dodawanie podswietlenia po najechaniu na dana opcje
-
                 this.nextElementSibling.children[j].addEventListener("mouseover", function(specifyProduct) {
                     this.classList.add("product-show");
                 });
@@ -141,10 +153,5 @@ document.addEventListener("DOMContentLoaded", function() {
                 });
             }
         });
-
     }
-
-
-
-
 });
